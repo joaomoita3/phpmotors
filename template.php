318bf14bb38ec92663339d6aface4802496d5ebe
Joao Moita <jmoita@fusionware.com>
2022-04-30 15:25:18 -0600

    test

---
+++ template.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" media="screen" href="/phpmotors/css/styles.css">
+</head>
+<body>
+  <div class="content-wrapper">
+    <header>
+      <div class="header-wrapper">
+        <img src="images/site/logo.png" alt="site-logo">
+        <div class="account">
+          <p>
+            <a href="#">
+              My Account
+            </a>
+          </p>
+        </div>
+      </div>
+    </header>
+    <nav>
+      <div class="nav-wrapper">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Classic</a></li>
+          <li><a href="#">Sports</a></li>
+          <li><a href="#">SUV</a></li>
+          <li><a href="#">Trucks</a></li>
+          <li><a href="#">Used</a></li>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <div class="main-wrapper">
+        <h1>
+          Content Title Here
+        </h1>
+      </div>
+    </main>
+    <hr>
+    <footer>
+      <div class="footer-wrapper">
+        <p>©PHP Motors, all rights reserved.</p>
+        <p>All images used are believed to be in "fair use". Please notify the author if they are not and they will be removed.</p>
+        <p>Last updated: 15 January , 2022</p>
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
